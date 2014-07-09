c1e4a4cad434874f925f1dc2ecc4889f8ea0d7d8
Erin Tomson <erin@integerlabs.net>
2014-07-09 10:20:55 -0700

    Added more stuff to the Wiring compatability layer.

--- Wiring.cpp
+++ Wiring.cpp
@@ -80,3 +80,77 @@ ISR(TIMER0_OVF_vect)
 	timer0_millis = m;
 	timer0_overflow_count++;
 }
+
+const uint16_t PROGMEM port_to_mode_PGM[] = {
+	NOT_A_PORT,
+	(uint16_t) &DDRA,
+	(uint16_t) &DDRB,
+};
+
+const uint16_t PROGMEM port_to_output_PGM[] = {
+	NOT_A_PORT,
+	(uint16_t) &PORTA,
+	(uint16_t) &PORTB
+};
+
+const uint16_t PROGMEM port_to_input_PGM[] = {
+	NOT_A_PORT,
+	(uint16_t) &PINA,
+	(uint16_t) &PINB
+};
+
+const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
+	PA, /* 0 */
+	PA,
+	PA,
+	PA,
+	PB,
+	PB,
+	PB,
+	PB,
+};
+
+const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
+	_BV(3),
+	_BV(2),
+	_BV(1),
+	_BV(0),
+	_BV(7),
+	_BV(2),
+	_BV(1),
+	_BV(0)
+};
+
+
+void pinMode(uint8_t pin, uint8_t mode)
+{
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+	volatile uint8_t *reg, *out;
+
+	if (port == NOT_A_PIN) return;
+
+	// JWS: can I let the optimizer do this?
+	reg = portModeRegister(port);
+	out = portOutputRegister(port);
+
+	if (mode == INPUT) {
+		uint8_t oldSREG = SREG;
+		cli();
+		*reg &= ~bit;
+		*out &= ~bit;
+		SREG = oldSREG;
+		} else if (mode == INPUT_PULLUP) {
+		uint8_t oldSREG = SREG;
+		cli();
+		*reg &= ~bit;
+		*out |= bit;
+		SREG = oldSREG;
+		} else {
+		uint8_t oldSREG = SREG;
+		cli();
+		*reg |= bit;
+		SREG = oldSREG;
+	}
+}
+
--- Wiring.h
+++ Wiring.h
@@ -5,10 +5,28 @@
 #define WIRING_H_
 
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 #include "Setup.h"
+
+#define PA 1
+#define PB 2
+#define PC 3
+#define PD 4
+#define PE 5
+#define PF 6
+#define PG 7
+#define PH 8
+#define PJ 10
+#define PK 11
+#define PL 12
+
+#define NOT_A_PIN 0
+#define NOT_A_PORT 0
+
 
 #define INPUT 0
 #define OUTPUT 1
+#define INPUT_PULLUP 0x2
 #define interrupts() sei()
 #define noInterrupts() cli()
 #define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
@@ -20,6 +38,25 @@
 #ifndef sbi
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
+
+extern const uint16_t PROGMEM port_to_mode_PGM[];
+extern const uint16_t PROGMEM port_to_input_PGM[];
+extern const uint16_t PROGMEM port_to_output_PGM[];
+
+extern const uint8_t PROGMEM digital_pin_to_port_PGM[];
+// extern const uint8_t PROGMEM digital_pin_to_bit_PGM[];
+extern const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[];
+extern const uint8_t PROGMEM digital_pin_to_timer_PGM[];
+
+void pinMode(uint8_t, uint8_t);
+
+#define digitalPinToPort(P) ( pgm_read_byte( digital_pin_to_port_PGM + (P) ) )
+#define digitalPinToBitMask(P) ( pgm_read_byte( digital_pin_to_bit_mask_PGM + (P) ) )
+#define digitalPinToTimer(P) ( pgm_read_byte( digital_pin_to_timer_PGM + (P) ) )
+#define analogInPinToBit(P) (P)
+#define portOutputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_output_PGM + (P))) )
+#define portInputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_input_PGM + (P))) )
+#define portModeRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_mode_PGM + (P))) )
 
 unsigned long micros();
 
